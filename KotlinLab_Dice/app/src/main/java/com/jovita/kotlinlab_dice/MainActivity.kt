3f06baac08a021ffc9763a2f2c2bd9486294ed41
jovitajohn641 <jovitajohn641@gmail.com>
2022-05-31 14:45:23 +0100

    completed dice rolling sample with unit test from google

--- KotlinLab_Dice/app/src/main/java/com/jovita/kotlinlab_dice/MainActivity.kt
+++ KotlinLab_Dice/app/src/main/java/com/jovita/kotlinlab_dice/MainActivity.kt
@@ -3,7 +3,9 @@ package com.jovita.kotlinlab_dice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +15,53 @@ class MainActivity : AppCompatActivity() {
 
         val rollButon: Button = findViewById(R.id.btn_roll)
         rollButon.setOnClickListener { rollDice() }
+
+        //Calling dice methord for an initial number
+        rollDice()
     }
 
-    /* rolls the dic andd displays number */
+    /* rolls the dic andd displays dice image */
     private fun rollDice() {
+        val myLuckyNumber = 2
         val myFirsDice = Dice(6)
-        val message: TextView = findViewById(R.id.txt_messaage)
+        val mySecondDice = Dice(6)
+        val firstDiceRoll = myFirsDice.rollDice()
+        val secondDiceRoll = mySecondDice.rollDice()
+
+        if (firstDiceRoll+secondDiceRoll == myLuckyNumber)
+            Toast.makeText(this, "Hooray! Its your Lucky number!!", Toast.LENGTH_SHORT).show()
+        setDiceUi(firstDiceRoll,secondDiceRoll)
+    }
+
+    fun setDiceUi(firstRoll:Int,secondRoll : Int){
+        // finding image resource for the random number
+        val firstImageResourcex = getImage(firstRoll)
+        val secondImageResource = getImage(secondRoll)
+
         //displaying num on screen
-        message.text = myFirsDice.rollDice().toString()
+        val diceImage1: ImageView = findViewById(R.id.img_first_dice)
+        diceImage1.setBackgroundResource(firstImageResourcex)
+        //content description
+        diceImage1.contentDescription = firstRoll.toString()
+
+        val diceImage2: ImageView = findViewById(R.id.img_second_dice)
+        diceImage2.setBackgroundResource(secondImageResource)
+        //content description
+        diceImage2.contentDescription = secondRoll.toString()
+    }
+
+    fun getImage(diceNumber : Int) : Int{
+        // finding image resource for the random number
+        val imageResource = when (diceNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.dice_1
+        }
+        return imageResource
     }
 }
 
--- KotlinLab_Dice/app/src/test/java/com/jovita/kotlinlab_dice/ExampleUnitTest.kt
+++ KotlinLab_Dice/app/src/test/java/com/jovita/kotlinlab_dice/ExampleUnitTest.kt
@@ -11,7 +11,9 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generatenumberFromDice() {
+        val dice = Dice(6)
+        val rolled = dice.rollDice()
+        assertTrue("The number is within dice sides",rolled in 1..6)
     }
 }
